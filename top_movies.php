ec794f5afe45d4bc4e29e2e233c00c016aadd342
Colleen <clm5uz@virginia.edu>
2017-03-15 22:48:12 -0400

    update query to display top five movies and shows on home page

--- top_movies.php
+++ top_movies.php
@@ -7,15 +7,20 @@
         return null;
     }
 
-    echo "<table class=\"table table-bordered\"><tr><th>Title</th><th>Year Released</th><th>Genre</th></tr>";
+    echo "<table class=\"table table-bordered\"><tr><th>Title</th><th>Year Released</th><th>Genre</th><th>Avg. Rating</th></tr>";
 
-    $result = mysqli_query($db_connection,"SELECT * FROM media NATURAL JOIN movie ORDER BY title");
+    $result = mysqli_query($db_connection, "SELECT media_id, title, year_released, genre, avg(star_rating) AS avg_rating
+						FROM media NATURAL JOIN movie NATURAL JOIN watched
+						GROUP BY media_id
+						ORDER BY avg_rating DESC
+						LIMIT 5");
 
     while($row = mysqli_fetch_array($result)) {
 	echo "<tr>";
         echo "<td>" . $row['title'] . "</td>";
         echo "<td>" . $row['year_released'] . "</td>";
         echo "<td>" . $row['genre'] . "</td>";
+	echo "<td>" . $row['avg_rating'] . "</td>";
         echo "</tr>";
     }
 
