4c390794105c9b7c36c00d0079a3dd95692ad8a3
Colleen <clm5uz@virginia.edu>
2017-04-19 14:41:49 -0400

    tv shows now have details

--- media/query_templates/EpisodeDetail.php
+++ media/query_templates/EpisodeDetail.php
@@ -9,34 +9,19 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-$str = $_POST['mediaID'];
-$se = $_POST['season'];
-$ep = $_POST['episode'];
+$str = 16;
 
 $results = $con->query(
-  "SELECT *, episode.title
-  AS 'episode_title', GROUP_CONCAT(first_name, ' ', last_name SEPARATOR ', ')
-  AS 'directors'
-  FROM episode
-  LEFT JOIN media
-  ON episode.media_id = media.media_id
-  LEFT JOIN directs
-  ON episode.media_id = directs.media_id
-  NATURAL JOIN director
-  WHERE episode.media_id = $str
-  AND season = $se
-  AND episode = $ep") or die ("Invalid Query: " . $con->error());
+  "SELECT *, GROUP_CONCAT(first_name, ' ', last_name SEPARATOR ', ') AS 'directors' FROM media NATURAL JOIN tv_show NATURAL JOIN directs NATURAL JOIN director WHERE media_id = $str") or die ("Invalid Query: " . $con->error());
 
 
 $num_rows = $results->num_rows;
 for ($row_iter = 0; $row_iter < $num_rows; $row_iter++) {
     $sql_row = $results->fetch_array();
     $title = $sql_row['title'];
-    $ep_title = $sql_row['episode_title'];
     $year = $sql_row['year_released'];
     $genre = $sql_row['genre'];
-    $season = $sql_row['season'];
-    $episode = $sql_row['episode'];
+    $seasons = $sql_row['number_of_seasons'];
     $rating = $sql_row['tv_rating'];
     $directors = $sql_row['directors'];
      echo "<h1><em>$title</em></h1>
@@ -44,8 +29,7 @@ for ($row_iter = 0; $row_iter < $num_rows; $row_iter++) {
 <p><strong>Director(s): $directors</strong></p>
 <p><strong>Released: $year&nbsp;</strong></p>
 <p><strong>Genre: $genre</strong></p>
-<p><strong>Season: $season</strong></p>
-<p><strong>Episode: $episode</strong></p>
+<p><strong>Number of Seasons: $seasons</strong></p>
 <p><strong>Rating: $rating</strong></p>";
 }
 
--- media/query_templates/TvShowDetail.php
+++ media/query_templates/TvShowDetail.php
@@ -1,33 +1,31 @@
 <?php
-include_once("./library.php");
-$con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-$str = $_POST['mediaID'];
-$sql="SELECT * FROM media";
-$results = $con->query("SELECT  title, year_released, genre, movie_rating, duration, GROUP_CONCAT(first_name, ' ', last_name SEPARATOR ', ') AS 'directors' FROM media NATURAL JOIN movie NATURAL JOIN directs NATURAL JOIN director WHERE media_id = $str
-") or die ("Invalid Query: " . $con->error());
+    require_once('library.php');
+    $db_connection = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+
+$str = 16;
+
+$results = mysqli_query($db_connection, "SELECT *, GROUP_CONCAT(first_name, ' ', last_name SEPARATOR ', ') AS 'directors'  FROM media NATURAL JOIN tv_show NATURAL JOIN directs NATURAL JOIN director WHERE media_id = $str;");
+
 $num_rows = $results->num_rows;
 for ($row_iter = 0; $row_iter < $num_rows; $row_iter++) {
     $sql_row = $results->fetch_array();
     $title = $sql_row['title'];
     $year = $sql_row['year_released'];
     $genre = $sql_row['genre'];
-    $rating = $sql_row['movie_rating'];
-    $duration = $sql_row['duration'];
+    $seasons = $sql_row['number_of_seasons'];
+    $rating = $sql_row['tv_rating'];
     $directors = $sql_row['directors'];
-    $lname = $sql_row['last_name'];
      echo "<h1><em>$title</em></h1>
 <p><strong>Director(s): $directors</strong></p>
 <p><strong>Released: $year&nbsp;</strong></p>
 <p><strong>Genre: $genre</strong></p>
-<p>Rating: $rating</p>
-<p>Duration: $duration</p>";
+<p><strong>Number of Seasons: $seasons</strong></p>
+<p><strong>Rating: $rating</strong></p>";
+
 }
-echo "hi";
+
+
+echo "</br> <h2><em>List of Episodes</em></h2></br>";
 
     echo "<table class=\"table table-bordered\"><tr><th>Title</th><th>Season</th><th>Episode</th><th>Duration</th></tr>";
 
@@ -44,6 +42,6 @@ echo "hi";
     }
 
     echo "</table>";
-
+echo "</br>";
 mysqli_close($con);
 ?>
