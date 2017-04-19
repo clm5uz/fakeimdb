0c55df757ff628731c5afc5e72b7b147e2aed498
Colleen <clm5uz@virginia.edu>
2017-04-19 13:44:44 -0400

    add password requirements, improve validation

--- js/signup.js
+++ js/signup.js
@@ -40,19 +40,34 @@ $(document).ready(function() {
 
 function validateSignupForm() {
 	alertString = "";
+	var firstName = document.getElementById('inputFirstname');
+	var lastName = document.getElementById('inputLastname');
+	var email = document.getElementById('inputEmail');
+	if (firstName.value === "" || lastName.value === "") {
+		alertString = "You must enter a full name.<br>";
+	}
+	var username = document.getElementById('inputUsername');
+        var usernameError = document.getElementById('usernameError');
+        if (username.value === "") {
+                alertString = alertString + "You must enter a username.<br>";
+        } else if (usernameError.style.display != "none") {
+                alertString = alertString + "You must choose a unique username.<br>";
+        }
+	if (email.value === "") {
+		alertString = alertString + "You must enter an email.<br>";
+	}
 	var password1 = document.getElementById('inputPassword1');
 	var password2 = document.getElementById('inputPassword2');
+	var passwordCheck = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
 	if (password1.value != password2.value) {
-		alertString = "Passwords must match.\n";
-	}
-
-	var usernameError = document.getElementById('usernameError');
-	if (usernameError.style.display != "none") {
-		alertString = alertString + "You must choose a unique username.\n";
+		alertString = alertString + "Passwords must match.<br>";
+	} else if (!password1.value.match(passwordCheck)) {
+		alertString = alertString + "Your password must be at least eight characters, and must contain:<ul><li>An uppercase letter</li><li>A lowercase letter</li><li>A number</li><li>A special character</li></ul>"
 	}
 
 	if (alertString != "") {
-		alert(alertString);
+		document.getElementById('modal_text').innerHTML = alertString;
+		$('#open_validate').click();
 		return false;
 	} else {
 		return true;
