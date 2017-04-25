d862bf8eb1642645e35e149739451c90894bcbde
mtfalls <goldjet45@gmail.com>
2017-04-25 16:20:49 -0400

    add to watch list now enabled on movies

--- media/movie-detail/movie-detail.component.js
+++ media/movie-detail/movie-detail.component.js
@@ -14,5 +14,32 @@ angular.
           self.movieData = response.data;
           //console.log("What's the word? " + self.movieData['title']);
         });
+        /* See if this media is already on your wants_to_watch list */
+        $http.post('query_templates/GetUserId.php').
+        then(function(response){
+          self.userId = response.data;
+          // Not logged in, don't show wants_to_watch button
+          if (self.userId.includes("-1")){
+            self.showButton = false;
+          }
+          else {
+            //console.log("Who am I?: " + self.userId);
+            self.showButton = true;
+            $http.post('query_templates/WatchStatus.php',
+            {"media_id": self.movieId, "user_id": self.userId}).
+            then(function(response){
+              self.watchResponse = response.data;
+              //console.log("What's the word? " + self.watchResponse['message']);
+            });
+          }
+        });
+        self.watchStatus = function watchStatus() {
+          $http.post('query_templates/WatchUpdate.php',
+          {"media_id": self.movieId, "user_id": self.userId}).
+          then(function(response){
+            self.watchResponse = response.data;
+            //console.log("NEW STATUS: " + self.watchResponse['message']);
+          });
+        };
     }]
   });
