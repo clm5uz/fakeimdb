3151a86caa83ddba3ca9c6b9327c8e8a2c6059e8
Colleen <clm5uz@virginia.edu>
2017-04-26 13:24:14 -0400

    logo on navbar

--- media/query_templates/ImageTest.php
+++ media/query_templates/ImageTest.php
@@ -1,8 +1,12 @@
 <?php
 include('simple_html_dom.php');
-$title = "Finding Nemo";
-$search_keyword=str_replace(' ','+',$title);
-$newhtml =file_get_html("https://www.google.com/search?q=".$search_keyword."&tbm=isch");
-$result_image_source = $newhtml->find('img', 0)->src;
-echo '<img src="'.$result_image_source.'">';
+
+    $search_query = "Finding Nemo";
+    $search_query = urlencode( $search_query );
+    $html = file_get_html( "https://www.google.com/search?q=$search_query&tbm=isch" );
+    $image_container = $html->find('img', 0)->src;
+        echo '<img src="'.$image_container.'" width="160" height="200">';
+
+
+
 ?>
